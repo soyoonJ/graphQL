3ed64e5f76fc81aa507b0c98032c9b4a671879d5
soyoonJ <sytkfkd96@gmail.com>
2023-07-23 10:28:42 +0900

    [graphQL]: rest api -> graphQL로 만들기

--- server.js
+++ server.js
@@ -23,28 +23,68 @@ let users = [
   { id: "2", firstname: "hodoo", lastname: "jj" },
 ];
 
+// 특정문자만 클릭 -> cmd + d
+// json -> graphQL : https://transform.tools/json-to-graphql
+// database 이미 있다면 hasura로 graphQL 쉽게 만들 수 있음 https://hasura.io/
 const typeDefs = gql`
   type User {
     id: ID!
     firstname: String!
     lastname: String!
+    """
+    Is the sum of firstname + lastname as a string
+    """
     fullname: String!
   }
+  """
+  Tweet object represents a resource for a Tweet
+  """
   type Tweet {
     id: ID
     text: String
     author: User
   }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    mpa_rating: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
+    state: String!
+    torrents: String!
+    date_uploaded: String!
+    date_uploaded_unix: String!
+  }
   type Query {
+    allMovies: [Movie!]!
     allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID): Tweet
+    movie(id: String!): Movie
   }
   type Mutation {
     postTweet(text: String, userId: ID): Tweet
     deleteTweet(id: ID!): Boolean
   }
 `;
+// Subscription type도 있음
 
 const resolvers = {
   Query: {
@@ -56,6 +96,16 @@ const resolvers = {
     },
     allUsers() {
       return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json").then((response) =>
+        response.json().then((json) => json.data.movies)
+      );
+    },
+    movie(_, { id }) {
+      return fetch(
+        `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+      ).then((response) => response.json().then((json) => json.data.movie));
     },
   },
   Mutation: {
